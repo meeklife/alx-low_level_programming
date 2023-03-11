fe342aea910ef8e18854e9896456907152668a7e
forson <forsonbaah@gmail.com>
2023-03-11 10:38:34 -0800

    added solutions to task 0,1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - program that prints the number of arguments passed into it
+*@argc: The program name
+*@argv: The number of command line arguments
+*Return: always 0
+*/
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
